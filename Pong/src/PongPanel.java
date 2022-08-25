7201d75e5a8c5d9c1db40ed41cad5f1c3d4a2714
aaditi3 <aaditi.dubale@gmail.com>
2022-08-25 18:59:49 +1200

    Module 3 'painting to the screen'

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -3,15 +3,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+//import javax.management.timer.Timer;
 import javax.swing.JPanel;
 import javax.swing.TransferHandler;
+import javax.swing.Timer;
 
   public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	  
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+			timer.start();
 	}	  
 	  
 	@Override
@@ -37,11 +45,21 @@ import javax.swing.TransferHandler;
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void update() {
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
-		
+		update();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
 	}
 
  }
