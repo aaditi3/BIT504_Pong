fd5afb07b68e6802889dd751463f2d796161d144
aaditi3 <aaditi.dubale@gmail.com>
2022-08-25 17:04:58 +1200

    Module 3 through 'PongPanel class'

---
+++ Pong/src/PongPanel.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.TransferHandler;
+
+  public class PongPanel extends JPanel implements ActionListener, KeyListener {
+
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	  
+	public PongPanel() {
+		setBackground(BACKGROUND_COLOUR);
+	}	  
+	  
+	@Override
+	public TransferHandler getTransferHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+ }
